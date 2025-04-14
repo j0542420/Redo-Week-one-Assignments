17e98dc4710e7fcae6cbe8ce4aad8c451177d7d8
Jose Villa <j0542420@amarillocollege.com>
2025-04-14 13:29:50 -0500

    Finish

--- Redo WK1Ex1/Program.cs
+++ Redo WK1Ex1/Program.cs
@@ -14,6 +14,8 @@ namespace Redo_WK1Ex1
             Console.WriteLine("This is the assignment from Week 1");
             // this prints out a welcome message
             Console.WriteLine("Welcome to ITSE1302");
+            // space between the title and the user input
+            Console.WriteLine();
 
             // prints out a title
             Console.WriteLine("Information Input");
@@ -21,20 +23,24 @@ namespace Redo_WK1Ex1
             Console.WriteLine("What is your First Name? ");
             // user inputs their first name
             string firstName = Console.ReadLine();
+
             // asking the user for their last name
             Console.WriteLine("What is your Last Name? ");
             // user inputs their last name
             string lastName = Console.ReadLine();
+
             // asking the user for their Age
             Console.WriteLine("What is your Age? ");
             // user inputs their age
             string age = Console.ReadLine();
+
             // asking the user for their Favorite Color
             Console.WriteLine("What is your Favorite Color? ");
             // user inputs their Favorite Color
             string color = Console.ReadLine();
+
             // prints out a meessage with the user's inputs
-            Console.WriteLine("Hello, " + firstName + " " + lastName + "! ");
+            Console.WriteLine("Hello, " + firstName + " " + lastName + "! You are " + age + " years old and your favorite color is " + color + ".");
         }
     }
 }
